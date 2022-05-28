38094cc15d80367e21c50d2c2b8f17b261ad60f7
pyungwook <pyungwook.hur@domfam.com>
2022-05-28 17:48:54 +0900

    async-waterfall 설치, async-waterfall 참고 index.js 파일 생성

---
+++ index.js
@@ -0,0 +1,19 @@
+(function(globals) {
+    'use strict';
+
+
+
+
+    
+    // define & exports & register func :: async waterfall - 참조
+
+    // if (typeof define !== 'undefined' && define.amd) {
+    //     define([], function () {
+    //         return waterfall;
+    //     }); // RequireJS
+    // } else if (typeof module !== 'undefined' && module.exports) {
+    //     module.exports = waterfall; // CommonJS
+    // } else {
+    //     globals.asyncWaterfall = waterfall; // <script>
+    // }
+})(this);
