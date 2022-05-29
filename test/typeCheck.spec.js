ee719eacd8d7ef8c176ebe6d102567aa3b69c6be
pyungwook <pyungwook.hur@domfam.com>
2022-05-29 13:41:10 +0900

    argsType.spec.js, typeCheck.spec.js

--- test/typeCheck.spec.js
+++ test/typeCheck.spec.js
@@ -9,10 +9,58 @@ const {
     isSymbol
 } = require('../js/typeCheck')
 
-// TODO: 테스트 코드 작성
+const str = [
+    'test', 'a01', '1234', '{ myTest }', '\n', '', 'undefined', 'null',
+    JSON.stringify({ a: 1, b: 5, c: 7}), 3 + ''
+]
+const num = [
+    123, 0, -12, -12.3, Math.random() * 3, NaN, Infinity, -Infinity,
+    parseInt('123'), parseInt('-123'), Number('abc')
+]
+const obj = [
+    {}, { a: 3, b: 5, c: 7 }, JSON.parse('{ "test" : "test" }')
+]
+const arr = [
+    [], [ 1, 3, 5 ], [ {}, {}, [] ]
+]
+const sym = [
+    Symbol(), Symbol.for('symKey')
+]
+const _null = [null]
+let _u
+const _undefined = [ undefined, _u, obj[0].a ]
 
-test('typeCheck', () => {
-    const input = ''
-    const output = 'test_007_A'
-    expect(input).toEqual(output)
+describe('typeCheck.js', () => {
+
+    test('isString', () => {
+        str.map(v => expect(isString(v)).toBeTruthy())
+    })
+    test('isNumber', () => {
+        num.map(v => expect(isNumber(v)).toBeTruthy())
+    })
+    test('isObject', () => {
+        obj.map(v => expect(isObject(v)).toBeTruthy())
+    })
+    test('isArray', () => {
+        arr.map(v => expect(isArray(v)).toBeTruthy())
+    })
+    test('isSymbol', () => {
+        sym.map(v => expect(isSymbol(v)).toBeTruthy())
+    })
+    test('isNull', () => {
+        _null.map(v => expect(isNull(v)).toBeTruthy())
+    })
+    test('isUndefined', () => {
+        _undefined.map(v => expect(isUndefined(v)).toBeTruthy())
+    })
+
+    test('getType', () => {
+        str.map(v => expect(getType(v)).toBe('string'))
+        num.map(v => expect(getType(v)).toBe('number'))
+        obj.map(v => expect(getType(v)).toBe('object'))
+        arr.map(v => expect(getType(v)).toBe('array'))
+        sym.map(v => expect(getType(v)).toBe('symbol'))
+        _null.map(v => expect(getType(v)).toBe('null'))
+        _undefined.map(v => expect(getType(v)).toBe('undefined'))
+    })
 })
