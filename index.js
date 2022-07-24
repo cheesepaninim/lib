4867378ccce64d76d6516b83ca6e4809c2630109
cheesepaninim <hpw0310@gmail.com>
2022-07-24 22:40:18 +0900

    .

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 (function(globals) {
     'use strict'
 
-    let chsp;
-    
-    chsp.argsType = require('./js/argsType');
-    chsp.convention = require('./js/convention');
-    chsp.typeCheck = require('./js/typeCheck');
-    chsp.pathAndValue = require('./js/pathAndValue');
-    chsp.waterfall = require('./js/waterfall');
+    const convention = require('./js/convention');
+    const typeCheck = require('./js/typeCheck');
+    const pathAndValue = require('./js/pathAndValue');
+    const argsType = require('./js/argsType');
+    const waterfall = require('./js/waterfall');
+
+    const chsp = {
+        convention,
+        typeCheck,
+        pathAndValue,
+        argsType,
+        waterfall
+    }
 
     // async-waterfall 참고
     if (typeof define !== 'undefined' && define.amd) {
